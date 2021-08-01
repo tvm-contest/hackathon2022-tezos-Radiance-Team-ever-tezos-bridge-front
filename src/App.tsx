5405a8f42ef197ec102e83dc9501012fa93c7ba6
Inokentii Mazhara <keshan3262@gmail.com>
2021-08-01 19:06:40 +0300

    Add 'Donate' button

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,10 @@ interface ConnectionState {
   tezos: TezosToolkit;
 }
 
+const CRUNCH_ADDRESS = "KT1BHCumksALJQJ8q8to2EPigPW6qpyTr7Ng";
+const CRUNCH_TOKEN_ID = 0;
+const AUTHOR_ADDRESS = "tz1LSMu9PugfVyfX2ynNU9y4eVvSACJKP7sg";
+
 function App() {
   const [connection, setConnection] = useState<ConnectionState>();
 
@@ -30,17 +34,42 @@ function App() {
       const tezos = wallet.toTezos();
       const { pkh, publicKey } = wallet.permission!;
       tezos.setSignerProvider(new ReadOnlySigner(pkh, publicKey));
-      setConnection({ tezos, accountPkh: await tezos.signer.publicKeyHash() });
+      setConnection({ tezos, accountPkh: await tezos.wallet.pkh() });
     } catch (e) {
       alert(`Error: ${e.message}`);
     }
   }, []);
+
+  const donateCrunch = useCallback(async () => {
+    try {
+      const { tezos, accountPkh } = connection!;
+      const tokenContract = await tezos.contract.at(CRUNCH_ADDRESS);
+      await tokenContract.methods
+        .transfer([
+          {
+            from_: accountPkh,
+            txs: [
+              {
+                to_: AUTHOR_ADDRESS,
+                token_id: CRUNCH_TOKEN_ID,
+                amount: 2e8,
+              },
+            ],
+          },
+        ])
+        .send();
+      alert("Thank you, the donation is being processed!");
+    } catch (e) {
+      alert(`Error while donating: ${e.message}`);
+    }
+  }, [connection]);
 
   return (
     <div>
       <button onClick={connectWallet}>
         {connection ? connection.accountPkh : "Connect to wallet"}
       </button>
+      {connection && <button onClick={donateCrunch}>Donate some CRUNCH</button>}
     </div>
   );
 }
