ddf39d42730199374b3b67fb540bc7773f2a2782
Ramazan Chasygov <theliketurbo@gmail.com>
2022-02-08 22:28:01 +0300

    feat: add storybook

---
+++ .storybook/main.js
@@ -0,0 +1,25 @@
+const {merge} = require("webpack-merge");
+const path = require("path");
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: "@storybook/react",
+  core: {
+    builder: "webpack5",
+  },
+  webpackFinal(config) {
+    return merge(config, {
+      resolve: {
+        alias: {
+          "@emotion/core": getPackageDir("@emotion/react"),
+          "@emotion/styled": getPackageDir("@emotion/styled"),
+        },
+      },
+    });
+  },
+};
+
+function getPackageDir(package) {
+  return path.dirname(require.resolve(path.join(package, "package.json")));
+}
