fde87c60122e3d846ab741c92ed7a36f773d59bf
Ramazan Chasygov <theliketurbo@gmail.com>
2022-03-04 19:26:07 +0300

    chore: update token names

--- src/misc/ever-tokens.ts
+++ src/misc/ever-tokens.ts
@@ -3,8 +3,8 @@ const everTokens = [
     address:
       "0:0d9b85d42c4824fb8b92ffc122168d92b431b6814686fa04af25277a987683a5",
     decimals: 9,
-    name: "DefiSpace DEX token",
-    symbol: "DEX",
+    name: "DeFiSpace DEX token",
+    symbol: "eDEX",
   },
 ];
 
--- src/misc/tezos-tokens.ts
+++ src/misc/tezos-tokens.ts
@@ -3,7 +3,7 @@ const tezosTokens = [
     contract: "KT1Ua1r4kEBUQ4vP546QyCc5WK6sadvesoPu",
     decimals: 9,
     is_transferable: true,
-    name: "test DefiSpace DEX token",
+    name: "DeFiSpace DEX token",
     network: "hangzhou2net",
     symbol: "tDEX",
     token_id: 0,
